31200d98817e68e659711a994b914d26ee6ed17b
Nicholas Brazeau <nbrazeau@med.unc.edu>
2020-05-04 12:32:31 -0400

    clean before merge overhaul

--- R/COVIDCurve.R
+++
@@ -1,9 +0,0 @@
-#------------------------------------------------
-#' @title COVIDCurve: Simple Inference of IFRs and I0 for Aggregate COVID Death Counts
-#'
-#' @description Assumes an incidence curve can be mapped on to a onset-to-death curve in order to infer
-#'              the IFR or I0 of an epidemic.
-#'
-#' @docType package
-#' @name COVIDCurve
-NULL
