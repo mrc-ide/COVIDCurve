c3de3b31ebda854434ea7ae5c670ddb2b194552b
Nicholas Brazeau <nbrazeau@med.unc.edu>
2020-06-05 17:13:12 -0400

    update docs and plot

--- R/summarize_plot.R
+++ R/summarize_plot.R
@@ -155,8 +155,7 @@ draw_posterior_infxn_points_cubic_splines <- function(IFRmodel_inf, whichrung = 
   fitcurve_start <- stringr::str_split_fixed(fitcurve_string, "const double OVERFLO_DOUBLE = DBL_MAX/100.0;", n = 2)[,1]
   fitcurve_start <- sub("SEXP", "Rcpp::List", fitcurve_start)
   fitcurve_curve <- stringr::str_split_fixed(fitcurve_string, "if \\(nodex_pass\\) \\{", n = 2)[,2]
-  fitcurve_curve <- stringr::str_split_fixed(fitcurve_curve, "bool spline_test = true;", n = 2)[,1]
-  #fitcurve_curve <- stringr::str_split_fixed(fitcurve_curve, "std::vector\\<double\\> cumm_infxn_spline\\(infxn_spline.size\\(\\)\\);", n = 2)[,1]
+  fitcurve_curve <- stringr::str_split_fixed(fitcurve_curve, "std::vector\\<double\\> cumm_infxn_spline\\(infxn_spline.size\\(\\)\\);", n = 2)[,1]
   fitcurve_string <- paste(fitcurve_start, fitcurve_curve, "Rcpp::List ret = Rcpp::List::create(infxn_spline); return ret;}", collapse = "")
   Rcpp::cppFunction(fitcurve_string)
 
