b5f23e19b4823c9f752a2181c4ba7829930133c9
Nicholas Brazeau <nbrazeau@med.unc.edu>
2020-06-11 19:56:15 -0400

    start to add multisero in

--- R/model.R
+++ R/model.R
@@ -36,7 +36,8 @@ make_IFRmodel_agg <- R6::R6Class(classname = "IFRmodel",
                                    sod = NULL,
                                    gamma_lookup = NULL,
                                    # sero items
-                                   Seroparams = NULL,
+                                   Serodayparams = NULL,
+                                   Serotestparams = NULL,
                                    popN = NULL,
                                    # censoring items
                                    rcensor_day = NULL,
@@ -46,13 +47,13 @@ make_IFRmodel_agg <- R6::R6Class(classname = "IFRmodel",
                                                          IFRparams = NULL, maxMa = NULL,
                                                          Infxnparams = NULL, relInfxn = NULL,
                                                          Knotparams = NULL, relKnot = NULL,
-                                                         Seroparams = NULL, popN = NULL,
+                                                         Serotestparams = NULL, Serodayparams = NULL, popN = NULL,
                                                          rcensor_day = NULL,
                                                          paramdf = NULL) {
                                      #......................
                                      # assertions and checks
                                      #......................
-                                     items <- c(data, level, IFRparams, Infxnparams, Knotparams, Seroparams, paramdf, pa, mod, sod, popN)
+                                     items <- c(data, level, IFRparams, Infxnparams, Knotparams, Serotestparams, Serodayparams, paramdf, pa, mod, sod, popN)
                                      if ( !all(sapply(items, is.null)) ) { # if user tries to input things, assert otherwise initialize empty -- N.B., we initialize gamma_lookup later based on knots
                                        #assert level
                                        assert_in(x = level, y = c("Time-Series", "Cumulative"))
@@ -73,15 +74,18 @@ make_IFRmodel_agg <- R6::R6Class(classname = "IFRmodel",
                                        assert_unique(Infxnparams)
                                        assert_string(Knotparams)
                                        assert_unique(Knotparams)
-                                       assert_string(Seroparams)
-                                       assert_unique(Seroparams)
-                                       assert_in(Seroparams, c("sens", "spec", "sero_day", "sero_rate"))
+                                       assert_string(Serotestparams)
+                                       assert_unique(Serotestparams)
+                                       assert_in(Serotestparams, c("sens", "spec", "sero_rate"))
+                                       assert_string(Serodayparams)
+                                       assert_unique(Serodayparams)
                                        # assert paramdf
                                        assert_dataframe(paramdf)
                                        assert_in(x = colnames(paramdf), y = c("name", "init", "min", "max", "dsc1", "dsc2"))
                                        assert_string(paramdf$name)
-                                       assert_in(paramdf$name, c(IFRparams, Infxnparams, Knotparams, Seroparams))
-                                       assert_in(c(IFRparams, Infxnparams, Knotparams, Seroparams), paramdf$name)
+                                       assert_unique(paramdf$name)
+                                       assert_in(paramdf$name, c(IFRparams, Infxnparams, Knotparams, Serotestparams, Serodayparams))
+                                       assert_in(c(IFRparams, Infxnparams, Knotparams, Serotestparams, Serodayparams), paramdf$name)
                                        assert_numeric(paramdf$init)
                                        assert_numeric(paramdf$min)
                                        assert_numeric(paramdf$max)
@@ -141,7 +145,8 @@ make_IFRmodel_agg <- R6::R6Class(classname = "IFRmodel",
                                        assert_in(relInfxn, Infxnparams)
                                        self$relInfxn <- relInfxn
                                      }
-                                     self$Seroparams <- Seroparams
+                                     self$Serotestparams <- Serotestparams
+                                     self$Serodayparams <- Serodayparams
                                      self$popN <- popN
                                      self$paramdf <- paramdf
                                      self$pa <- pa
@@ -239,13 +244,19 @@ make_IFRmodel_agg <- R6::R6Class(classname = "IFRmodel",
                                      self$relInfxn <- val
                                    },
 
-                                   set_Seroparams = function(val) {
+                                   set_Serotestparams = function(val) {
                                      assert_string(val)
                                      assert_unique(val)
-                                     assert_in(val, c("sens", "spec", "sero_rate", "sero_day"),
+                                     assert_in(val, c("sens", "spec", "sero_rate"),
                                                message = "Serology parameters currently limited to specifitiy (spec),
-                                             sensitivity (sens), serology rate (sero_rate) and date of serology rate (sero_day)")
-                                     self$Seroparams <- val
+                                             sensitivity (sens), and serology rate (sero_rate)")
+                                     self$Serotestparams <- val
+                                   },
+
+                                   set_Serodayparams = function(val) {
+                                     assert_string(val)
+                                     assert_unique(val)
+                                     self$Serodayparams <- val
                                    },
 
                                    set_paramdf = function(val) {
@@ -255,7 +266,8 @@ make_IFRmodel_agg <- R6::R6Class(classname = "IFRmodel",
                                      assert_dataframe(val)
                                      assert_in(x = colnames(val), y = c("name", "init", "min", "max", "dsc1", "dsc2"))
                                      assert_string(val$name)
-                                     assert_in(val$name, c(self$IFRparams, self$Knotparams, self$Infxnparams, self$Seroparams))
+                                     assert_unique(val$name)
+                                     assert_in(val$name, c(self$IFRparams, self$Knotparams, self$Infxnparams, self$Serotestparams, self$Serodayparams))
                                      assert_numeric(val$init)
                                      assert_numeric(val$min)
                                      assert_numeric(val$max)
