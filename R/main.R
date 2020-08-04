55b50c2f4833716abc859da941e93beaf2276d2a
Nicholas Brazeau <nbrazeau@med.unc.edu>
2020-08-04 13:48:25 -0400

    fix reparam of serorate for udpated prior

--- R/main.R
+++ R/main.R
@@ -187,10 +187,12 @@ run_IFRmodel_agg <- function(IFRmodel, reparamIFR = TRUE, reparamInfxn = TRUE, r
     mcmcout$output[, liftovercols] <- sapply(liftovercols.list, function(x) {x * mcmcout$output[, relInfxn]})
   }
 
-  #......................
-  # reparameterize sero_rate
-  #......................
-  mcmcout$output$sero_rate <- mcmcout$output[, IFRmodel$modparam] * mcmcout$output$sero_rate
+  if (reparamSeroRate) {
+    #......................
+    # reparameterize sero_rate
+    #......................
+    mcmcout$output$sero_rate <- mcmcout$output[, IFRmodel$modparam] * mcmcout$output$sero_rate
+  }
 
   # store input along with Dr.Jacoby output for later use
   inputs <- list(
