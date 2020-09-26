43a3b649e3dde5da7addd2941e382ae974e7e282
nickbrazeau <nbrazeau1@gmail.com>
2020-09-26 01:18:12 -0400

    update summarize plot for binomail v logit infxn curve

--- R/summarize_plot.R
+++ R/summarize_plot.R
@@ -333,13 +333,13 @@ draw_posterior_infxn_cubic_splines <- function(IFRmodel_inf, whichrung = "rung1"
                   obs_serologyn = IFRmodel_inf$inputs$IFRmodel$data$obs_serology$SeroN)
   } else {
     # logit-normal transformation for likelihood
-    IFRmodel$data$obs_serology <- IFRmodel$data$obs_serology %>%
+    IFRmodel_inf$inputs$IFRmodel$data$obs_serology <- IFRmodel_inf$inputs$IFRmodel$data$obs_serology %>%
       dplyr::mutate(SeroSE = (COVIDCurve:::logit(SeroPrevUCI) - COVIDCurve:::logit(SeroPrevLCI)) / (2*1.96) )
 
-    datin <- list(obs_deaths = IFRmodel$data$obs_deaths$Deaths,
-                  prop_strata_obs_deaths = IFRmodel$data$prop_deaths$PropDeaths,
-                  obs_serologymu = IFRmodel$data$obs_serology$SeroMu,
-                  obs_serologysigma = IFRmodel$data$obs_serology$SeroSE)
+    datin <- list(obs_deaths = IFRmodel_inf$inputs$IFRmodel$data$obs_deaths$Deaths,
+                  prop_strata_obs_deaths = IFRmodel_inf$inputs$IFRmodel$data$prop_deaths$PropDeaths,
+                  obs_serologymu = IFRmodel_inf$inputs$IFRmodel$data$obs_serology$SeroMu,
+                  obs_serologysigma = IFRmodel_inf$inputs$IFRmodel$data$obs_serology$SeroSE)
   }
 
   #......................
