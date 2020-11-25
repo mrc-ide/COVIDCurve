1b7cf485babc8927cedc757fbb99dfe831771a71
nickbrazeau <nbrazeau@med.unc.edu>
2020-11-25 12:24:42 -0500

    update docs

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,9 @@
 #------------------------------------------------
 #' @title COVIDCurve: Simple Inference of Age-Specific IFRs
-#'
+#' @description In many settings, individual-based (i.e. "line-list") data are often unavailable or
+#' unreliable. Here we provide a model framework that relies on aggregated death counts and seroprevalence
+#' data to infer the infection fatality ratio, or the probability of death givin infection. Models are
+#' fit within a Bayesian framework and account for onset-outcome delays and serologic test characteristics.
 #'
 #' @docType package
 #' @name COVIDCurve
